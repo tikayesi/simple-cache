665fff6d18630a2bfae8c314e1ddb690b45c66f1
Tika Yesi Kristiani <yesikristianitika@gmail.com>
2023-04-10 07:47:52 +0700

    initial commit

---
+++ src/main/java/com/enigma/advancedexp/AdvancedExpApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.advancedexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedExpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enigma/advancedexp/AdvancedExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.advancedexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedExpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
